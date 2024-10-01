52578446b05d1a6ee4010265b3696930093f2978
samuel.mugisha <samuel.mugisha@codeland.it>
2024-10-01 19:35:52 +0200

    refactor fabfile

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,54 +1,64 @@
-#!/usr/bin/python3
-"""
-Fabric script based on the file 2-do_deploy_web_static.py that creates and
-distributes an archive to the web servers
-
-execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
-"""
-
-from fabric.api import env, local, put, run
+from fabric import task
+from fabric.connection import Connection
+from invoke import run as local_run  # For local commands
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['54.242.122.123', '54.147.161.62']
 
+# Define the hosts and user details
+env_hosts = ['54.242.122.123', '54.147.161.62']
+user = 'ubuntu'
 
-def do_pack():
-    """generates a tgz archive"""
+@task
+def do_pack(c):
+    """Generates a tgz archive"""
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
+        if not isdir("versions"):
+            local_run("mkdir versions")
+        file_name = f"versions/web_static_{date}.tgz"
+        local_run(f"tar -cvzf {file_name} web_static")
+        print(f"Archive created: {file_name}")
         return file_name
-    except:
+    except Exception as e:
+        print(f"Error in do_pack: {e}")
         return None
 
-
-def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+@task
+def do_deploy(c, archive_path):
+    """Distributes an archive to the web servers"""
+    if not exists(archive_path):
+        print(f"Archive path does not exist: {archive_path}")
         return False
     try:
         file_n = archive_path.split("/")[-1]
         no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        c.put(archive_path, '/tmp/')
+        c.run(f'mkdir -p {path}{no_ext}/')
+        c.run(f'tar -xzf /tmp/{file_n} -C {path}{no_ext}/')
+        c.run(f'rm /tmp/{file_n}')
+        c.run(f'mv {path}{no_ext}/web_static/* {path}{no_ext}/')
+        c.run(f'rm -rf {path}{no_ext}/web_static')
+        c.run(f'rm -rf /data/web_static/current')
+        c.run(f'ln -s {path}{no_ext}/ /data/web_static/current')
+        print(f"Deployed {file_n} to {path}{no_ext}/")
         return True
-    except:
+    except Exception as e:
+        print(f"Error in do_deploy: {e}")
         return False
 
-
-def deploy():
-    """creates and distributes an archive to the web servers"""
-    archive_path = do_pack()
-    if archive_path is None:
+@task
+def deploy(c):
+    """Creates and distributes an archive to the web servers"""
+    archive_path = do_pack(c)
+    if not archive_path:
+        print("do_pack failed")
         return False
-    return do_deploy(archive_path)
+
+    for host in env_hosts:
+        conn = Connection(host=host, user=user, connect_kwargs={"key_filename": "~/.ssh/school"})
+        if not do_deploy(conn, archive_path):
+            print(f"Failed to deploy to {host}")
+            return False
+    print("Deployment complete")
+    return True
